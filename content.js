4307cacc957930cd4422c629f9c3b03d5e76ef11
Jhowl <jhowl@fuerzastudio.com.br>
2023-03-13 14:59:12 -0400

    Update open ai API

--- content.js
+++ content.js
@@ -11,17 +11,14 @@ export const generateContent = async (content, hashtag) => {
   const prompt = `I want you to act as a social media manger for Instagram and help me with creating content  to a post for my audience based on the content I provide, create in the end of the content emotes for the post and hashtags. I will give you some content. Please only provide content using content suggestions. Generate content in portuguese brazilian, with focus on the brazilian people who use instagram. Here is the first content: "${content}"${contentHastag}`
   console.log(prompt)
   try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
-      temperature: 0.9,
-      max_tokens: 1200,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      // stop: '\n'
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: prompt}],
     })
-    return response.data.choices[0].text
+
+    console.log(response.data)
+
+    return response.data.choices[0].message.content
   } catch (error) {
     console.error(error)
     return 'Error generating content.'
